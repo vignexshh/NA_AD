75d0db2c66011d5e9ec81d403a1bc8086a9a79dd
vigneshtd <vigneshtd111@gmail.com>
2025-02-16 20:35:11 +0530

    changed grid

--- src/app/analyse-rank/page.tsx
+++ src/app/analyse-rank/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Flex, Radio, Card, Space, Select, Button, Table } from "antd";
+import { Flex, Radio, Card, Space, Select, Button, Table, InputNumber } from "antd";
 import type { ColumnsType } from "antd/es/table";
-
 // Import JSON file (adjust path as needed)
 import data from "./2_ALL INDIA QUOTA AYUSH ROUND-1 ALLOTMENTS.json";
 
@@ -14,6 +13,7 @@ const App: React.FC = () => {
     course: [],
     allottedCategory: undefined,
     candidateCategory: undefined,
+    maxRank: undefined, // New state for max rank
   });
 
   // State for filtered data
@@ -55,7 +55,8 @@ const App: React.FC = () => {
         (filters.allottedCategory === undefined ||
           filters.allottedCategory === item["Alloted Category"]) &&
         (filters.candidateCategory === undefined ||
-          filters.candidateCategory === item["Candidate Category"])
+          filters.candidateCategory === item["Candidate Category"]) &&
+        (filters.maxRank === undefined || item.Rank <= filters.maxRank) // Add max rank condition
       );
     });
     setFilteredData(filtered);
@@ -69,6 +70,7 @@ const App: React.FC = () => {
       course: [],
       allottedCategory: undefined,
       candidateCategory: undefined,
+      maxRank: undefined, // Reset max rank
     });
   };
 
@@ -89,109 +91,127 @@ const App: React.FC = () => {
       <Flex gap="middle" vertical>
         {/* Filters Card */}
         <Card title="Available Filters & Data Tweaks">
-          <Space direction="vertical" style={{ width: "100%"  , overflowX: "auto"  }} >
-          <div className="choice-01 flex allign-center "> 
-           
-            <p className="font-bold pr-5 md:pr-10"> NEET Quota: </p>
-            <Select
-              // mode="multiple"
-              // maxTagCount={'responsive'}
-              allowClear
-              placeholder="Select Allotted Quota"
-              value={filters.allottedQuota}
-              onChange={(value) => handleFilterChange("allottedQuota", value)}
-              options={uniqueValues.allottedQuota.map((quota) => ({
-                value: quota,
-                label: quota,
-              }))}
-              // style={{ width: "100%" }}
-            />
-             </div>
-            
-
-            {/* Allotted Institute */}
-            <div className="choice-01 flex allign-center "> 
-           
-            <p className="font-bold pr-5 md:pr-10"> NEET Quota: </p>
-            <Select
-              showSearch
-              allowClear
-              placeholder="Select Allotted Institute"
-              value={filters.allottedInstitute}
-              onChange={(value) => handleFilterChange("allottedInstitute", value)}
-              options={uniqueValues.allottedInstitute.map((institute) => ({
-                value: institute,
-                label: institute,
-              }))}
-              
-            />
-           
-            
+          <div className="flex flex-col md:flex-row gap-2 pb-5">
+            <Space direction="vertical" style={{ width: "100%", overflowX: "auto" }}>
+              {/* Max Rank Input */}
+              <div className="choice-01 flex items-center gap-2">
+                <p className="font-bold pr-5 md:pr-10">Max Rank:</p>
+                <InputNumber
+                  placeholder="Enter Max Rank"
+                  value={filters.maxRank}
+                  onChange={(value) => handleFilterChange("maxRank", value)}
+                  min={1} // Minimum rank is 1
 
 
-            {/* Course */}
-            <Select
-              mode="multiple"
-              maxTagCount={'responsive'}
-              allowClear
-              placeholder="Select Course"
-              value={filters.course}
-              onChange={(value) => handleFilterChange("course", value)}
-              options={uniqueValues.course.map((course) => ({
-                value: course,
-                label: course,
-              }))}
-              style={{ width: "100%" }}
-            />
-            </div>
+                />
+              </div>
 
+              {/* Allotted Quota */}
+              <div className="choice-01 flex items-center gap-2">
+                <p className="font-bold pr-5 md:pr-10">Quota:</p>
+                <Select
+                  allowClear
+                  placeholder="Select Allotted Quota"
+                  value={filters.allottedQuota}
+                  onChange={(value) => handleFilterChange("allottedQuota", value)}
+                  options={uniqueValues.allottedQuota.map((quota) => ({
+                    value: quota,
+                    label: quota,
+                  }))}
+                />
+              </div>
 
-            {/* Allotted Category */}
-            <Select
-              showSearch
-              allowClear
-              placeholder="Select Allotted Category"
-              value={filters.allottedCategory}
-              onChange={(value) => handleFilterChange("allottedCategory", value)}
-              options={uniqueValues.allottedCategory.map((category) => ({
-                value: category,
-                label: category,
-              }))}
-              style={{ width: "100%" }}
-            />
+              {/* Allotted Institute */}
+              <div className="choice-01 flex items-center gap-2">
+                <p className="font-bold pr-5 md:pr-10">College:</p>
+                <Select
+                  showSearch
+                  allowClear
+                  placeholder="Select Allotted Institute"
+                  value={filters.allottedInstitute}
+                  onChange={(value) => handleFilterChange("allottedInstitute", value)}
+                  options={uniqueValues.allottedInstitute.map((institute) => ({
+                    value: institute,
+                    label: institute,
+                  }))}
+                  style={{ width: "100%" }}
+                />
+              </div>
 
-            {/* Candidate Category */}
-            <Select
-              showSearch
-              allowClear
-              placeholder="Select Candidate Category"
-              value={filters.candidateCategory}
-              onChange={(value) => handleFilterChange("candidateCategory", value)}
-              options={uniqueValues.candidateCategory.map((category) => ({
-                value: category,
-                label: category,
-              }))}
-              style={{ width: "100%" }}
-            />
+              {/* Course */}
+              <div className="choice-01 flex items-center gap-2">
+                <p className="font-bold pr-5 md:pr-10">Course:</p>
+                <Select
+                  allowClear
+                  placeholder="Select Course"
+                  value={filters.course}
+                  onChange={(value) => handleFilterChange("course", value)}
+                  options={uniqueValues.course.map((course) => ({
+                    value: course,
+                    label: course,
+                  }))}
+                />
+              </div>
+            </Space>
 
-<Button danger type="primary" onClick={clearFilters} style={{ marginBottom: 16 }}>
-              Clear All Filters
-            </Button>
-            
-          </Space>
+            <Space direction="vertical" style={{ width: "100%", overflowX: "auto" }}>
+              {/* Allotted Category */}
+              <div className="choice-01 flex items-center gap-2">
+                <p className="font-bold pr-5 md:pr-10">Category:</p>
+                <Select
+                  showSearch
+                  allowClear
+                  placeholder="Select Allotted Category"
+                  value={filters.allottedCategory}
+                  onChange={(value) => handleFilterChange("allottedCategory", value)}
+                  options={uniqueValues.allottedCategory.map((category) => ({
+                    value: category,
+                    label: category,
+                  }))}
+                  style={{ width: "100%" }}
+                />
+              </div>
+
+              {/* Candidate Category */}
+              <div className="choice-01 flex items-center gap-2">
+                <p className="font-bold pr-5 md:pr-10">Candidate_Category:</p>
+                <Select
+                  showSearch
+                  allowClear
+                  placeholder="Select Candidate Category"
+                  value={filters.candidateCategory}
+                  onChange={(value) => handleFilterChange("candidateCategory", value)}
+                  options={uniqueValues.candidateCategory.map((category) => ({
+                    value: category,
+                    label: category,
+                  }))}
+                  style={{ width: "100%" }}
+                />
+              </div>
+            </Space>
+          </div>
+
+          <Button danger type="primary" onClick={clearFilters} style={{ marginBottom: 16 }}>
+            Clear All Filters
+          </Button>
+
+        <div className="flex flex-col w-full"> 
+          <div className="pt-5 pb-5 border-black border-2 "> <p> s x s</p> </div> 
+          
+          </div>       
+         
 
         </Card>
 
         {/* Data Table Card */}
         <Card>
-        <div style={{ overflowX: "auto" }}>
-            
+          <div style={{ overflowX: "auto" }}>
             <Table
               dataSource={filteredData}
               columns={columns}
               rowKey="SNo"
               pagination={{ pageSize: 10 }}
-              style={{ minHeight: 100}}
+              style={{ minHeight: 100 }}
             />
           </div>
         </Card>
